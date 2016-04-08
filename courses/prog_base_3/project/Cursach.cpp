62830a2ff84ba3b455ad1f90ed6eef4e99513f90
Armen-Arakelian <yozhyn1@gmail.com>
2016-04-08 20:13:56 +0300

    Animation

--- courses/prog_base_3/project/Cursach.cpp
+++ courses/prog_base_3/project/Cursach.cpp
@@ -1,42 +1,75 @@
 #include "stdafx.h"
+#include <stdio.h>
 #include <GL/glut.h>
 
-void Reshape(int width, int height)
+int WindW, WindH;
+int i;
+int alpha;
+
+void Reshape(int width, int height) 
 {
 	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
+	gluOrtho2D(-1, 1, -1, 1);
 	glMatrixMode(GL_MODELVIEW);
+
+	WindW = width;
+	WindH = height;
 }
 
-void Display(void)
+void Display(void) 
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glLineWidth(1);
+	glLineWidth(3);
+	glColor3f(0.0f, 0.6f, 0.9f);
 
+	glPushMatrix();
+	glRotatef(alpha, 0.0f, 0.0f, 1.0f);
+	alpha += 5;
+	if (alpha > 359) 
+		alpha = 0;
 	glBegin(GL_LINES);
-	glVertex3f(0, 0.5f, 1);
-	glVertex3f(0, -0.5f, 1);
+	glVertex3f(-0.5f, 0.5f, 0.7f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
 	glEnd();
+	glPopMatrix();
 
 	glFlush();
+	glutSwapBuffers();
+}
+
+void Visibility(int state) 
+{
+	if (state == GLUT_NOT_VISIBLE) printf("Window not visible!\n");
+	if (state == GLUT_VISIBLE) printf("Window visible!\n");
+}
+
+void timf(int value) 
+{
+	glutPostRedisplay();  
+	glutTimerFunc(40, timf, 0); 
 }
 
 int main(int argc, char *argv[])
 {
+	WindW = 400;
+	WindH = 300;
+	alpha = 0;
+
 	glutInit(&argc, argv);
-	glutInitWindowSize(400, 300);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WindW, WindH);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutCreateWindow("buiviuh");
 
-	glutInitDisplayMode(GLUT_RGB);
-	glutCreateWindow("gbtrbb");
-
-	glutReshapeFunc(Reshape);
-	glutDisplayFunc(Display);
+	glutReshapeFunc(Reshape); 
+	glutDisplayFunc(Display);    
+	glutTimerFunc(40, timf, 0); 
+	glutVisibilityFunc(Visibility); 
 	glClearColor(0, 0, 0, 0);
 
 	glutMainLoop();
+
 	return 0;
 }
