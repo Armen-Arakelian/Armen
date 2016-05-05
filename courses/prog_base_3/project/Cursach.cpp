77bc3496aba66573776ca292ffc93a1983c24ea5
Armen-Arakelian <yozhyn1@gmail.com>
2016-05-05 15:07:22 +0300

    creating obstacles for runner. 1.0

--- courses/prog_base_3/project/Cursach.cpp
+++ courses/prog_base_3/project/Cursach.cpp
@@ -1,17 +1,20 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <GL/glut.h>
+#include <math.h>
 
 int WindW, WindH;
 int i;
-int alpha;
+double alpha = 0;
+double beta = 0.1;
 
 void Reshape(int width, int height) 
 {
 	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(100, 100, 100, 100);
+	gluPerspective(130, 1, 50, 0);
+	gluLookAt(1.0, 1.5, 2.0, 1.0, 1.5, -10.0, 0.0, 1.0, 0.0);
 
 	WindW = width;
 	WindH = height;
@@ -25,17 +28,27 @@ void Display(void)
 	glColor3f(0.0f, 0.6f, 0.9f);
 
 	glPushMatrix();
-	glRotatef(alpha, alpha, 0.0f, 1.0f);
-	alpha += 5;
-	if (alpha > 359) 
-		alpha = 0;
+	glTranslatef(1, 1, alpha);
+	alpha += beta;
+	if (alpha > 1 || alpha < -1) 
+		beta = -beta;
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.5f, 0.5f, 0.7f);
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glVertex3f(-0.1f, 0.0f, 0.3f);
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
 	glBegin(GL_LINES);
-	glVertex3f(-0.5f, 0.5f, 0.7f);
-	glVertex3f(0.5f, -0.5f, 0.5f);
-	glVertex3f(-0.2f, 0.2f, 0.3f);
-	glVertex3f(0.2f, -0.2f, 0.4f);
-	glVertex3f(-0.1f, 0.1f, 0.15f);
-	glVertex3f(0.1f, -0.1f, 0.2f);
+	glVertex3f(2.0f, 0.5f, 0.0f);
+	glVertex3f(2.0f, 0.5f, 100.0f);
+	glVertex3f(1.4f, 0.5f, 0.0f);
+	glVertex3f(1.4f, 0.5f, 100.0f);
+	glVertex3f(0.7f, 0.5f, 0.0f);
+	glVertex3f(0.7f, 0.5f, 100.0f);
+	glVertex3f(0.0f, 0.5f, 0.0f);
+	glVertex3f(0.0f, 0.5f, 100.0f);
 	glEnd();
 	glPopMatrix();
 
